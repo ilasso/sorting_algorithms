9bab1c78453a4b024a302c988608e2a19d715b90
ilasso <1204@holbertonschool.com>
2020-02-03 18:24:17 +0000

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * selection_sort - implement sort selection algorithm
+ *
+ * @array: The array to be ordered
+ * @size: Number of elements in @array
+ */
+void selection_sort(int *array, size_t size)
+{
+
+	int temp;
+	unsigned long int i;
+	unsigned long int j;
+	int smaller;
+
+	smaller = 0;
+	for (i = 0; i <= (size - 1); i++)
+	{
+		smaller = i;
+		for (j = i; j <= (size - 1); j++)
+		{
+			if (array[i] > array[j] && array[j] < array[smaller])
+			{
+				smaller = j;
+			}
+		}
+		if (array[smaller] != array[i])
+		{
+			temp = array[smaller];
+			array[smaller] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
+return;
+}
--- sort.h
+++ sort.h
@@ -20,4 +20,5 @@ void bubble_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
 int swapNode(listint_t **prev, listint_t **pos, listint_t **list);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 #endif
